a3e96d2f3662781a6047342a1a9622e932fd64ef
Shahriar Turag <shahriarturag.st@gmail.com>
2023-09-21 22:42:57 +0600

    created navbar and header

---
+++ client/app/components/Hero.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {};
+
+const Hero: React.FC<Props> = (props) => {
+	return (
+		<div className=' flex min-h-screen items-center justify-center'>
+			<div className='hero-animation absolute h-[50vh] w-[50vh] rounded-full' />
+			<div className='text-center space-y-10 z-10 800px:px-0 px-10'>
+				<h1 className='max-w-[1100px] 800px:text-6xl text-4xl font-extrabold dark:text-white text-black'>
+					Improve your online{' '}
+					<span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-400 '>
+						Learning Experience
+					</span>{' '}
+					Better Instantly.
+				</h1>
+				<p className='max-w-[800px] mx-auto text-slate-400 800px:text-2xl text-md'>
+					Empower your academic journey with Brain Boost&apos;s
+					dedicated community and comprehensive resources.
+				</p>
+				<Link href={'/courses'}>
+					<button className='font-bold text-white bg-blue-400 px-5 py-3 rounded-full mt-5'>
+						Explore Courses
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
---
+++ client/app/utils/NavItem.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export const navItemsData = [
+	{
+		name: 'Home',
+		url: '/',
+	},
+	{
+		name: 'Courses',
+		url: '/courses',
+	},
+	{
+		name: 'About',
+		url: '/about',
+	},
+	{
+		name: 'Policy',
+		url: '/policy',
+	},
+	{
+		name: 'FAQ',
+		url: '/faq',
+	},
+];
+
+type Props = {
+	activeItem: number;
+	isMobile: boolean;
+};
+
+const NavItem: React.FC<Props> = ({ activeItem, isMobile }) => {
+	return (
+		<>
+			<div className='hidden 800px:flex'>
+				{navItemsData &&
+					navItemsData.map((i, index) => (
+						<Link href={`${i.url}`} key={index} passHref>
+							<span
+								className={`${
+									activeItem === index
+										? 'dark:text-[#37a39a] text-[crimson]'
+										: 'dark:text-white text-black'
+								} text-[18px] px-6 font-poppins font-[400]`}
+							>
+								{i.name}
+							</span>
+						</Link>
+					))}
+			</div>
+			{isMobile && (
+				<div className='800px:hidden mt-5'>
+					<div className='w-full flex items-center justify-center'>
+						<Link href={'/'} passHref>
+							<Image
+								src='/logo.svg'
+								alt='logo'
+								width={180}
+								height={180}
+							/>
+						</Link>
+					</div>
+					{navItemsData &&
+						navItemsData.map((i, index) => (
+							<Link href='/' passHref key={index}>
+								<span
+									className={`${
+										activeItem === index
+											? 'dark:text-[#37a39a] text-[crimson]'
+											: 'dark:text-white text-black'
+									} py-5 text-[18px] px-6 font-poppins font-[400] block`}
+								>
+									{i.name}
+								</span>
+							</Link>
+						))}
+				</div>
+			)}
+		</>
+	);
+};
+
+export default NavItem;
